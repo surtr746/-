485ee82a21216fe42797b5825f5aec2e06e86f8e
surtr746 <cjia3438@gmail.com>
2025-06-05 21:47:55 +0800

    Update Card.h

--- Card.h
+++ Card.h
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #ifndef CARD_H
 #define CARD_H
 #include<string>
@@ -27,30 +26,4 @@ public:
 	string getType(int i);
 };
 #endif
-=======
-#ifndef CARD_H
-#define CARD_H
-#include<string>
-using namespace std;
-class Card {
-private:
-	int light=0;
-	int typeCount=1;
-	string name;
-	string* type=nullptr;
-	int* scope =nullptr;
-public:
-	Card();
-	Card(int,int,string);
-	void setType(string,int);
-	void setScope(int, int,int);
-	int getScope(int)const;
-	Card(const Card& other);
-	Card& operator=(const Card& other);
-	~Card();
-	void printCard();
-	int randNum(int,int);
-	int getTypeCount()const;
-};
-#endif
->>>>>>> 071d210843a89531f7dd01ac0ae4a246b4a36857
+
